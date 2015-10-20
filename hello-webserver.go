8c6b8730e404f8e49dbecfbc77e801ddd9ffd8b0
Johannes Scheuermann <johannes.scheuermann@inovex.de>
2015-10-20 16:12:42 +0200

    Run golint - style fixes

--- hello-webserver.go
+++ hello-webserver.go
@@ -1,42 +1,47 @@
 package main
 
 import (
-    "io"
-    "net/http"
-    "net"
-    "log"
-    "os"
-    "io/ioutil"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
 var (
-    Trace   *log.Logger
-    Info    *log.Logger
-    Warning *log.Logger
-    Error   *log.Logger
+	//Trace logger
+	Trace *log.Logger
+	//Info logger for Info logs
+	Info *log.Logger
+	//Warning logger for warning logs
+	Warning *log.Logger
+	//Error logger for error logs
+	Error *log.Logger
 )
 
+//Init all different logger
 func Init(
-    traceHandle io.Writer,
-    infoHandle io.Writer,
-    warningHandle io.Writer,
-    errorHandle io.Writer) {
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
 
-    Trace = log.New(traceHandle,
-        "TRACE: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 
-    Info = log.New(infoHandle,
-        "INFO: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 
-    Warning = log.New(warningHandle,
-        "WARNING: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 
-    Error = log.New(errorHandle,
-        "ERROR: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func responseWithIPs(w http.ResponseWriter, r *http.Request) {
@@ -50,21 +55,21 @@ func responseWithIPs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, i := range ifaces {
-	    addrs, err := i.Addrs()
-	    if err != nil {
-		    http.Error(w, err.Error(), 500)
-		    Error.Println(err.Error())
-	    }
+		addrs, err := i.Addrs()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			Error.Println(err.Error())
+		}
 
-	    for _, addr := range addrs {
-            io.WriteString(w, addr.String()+"\n")
-	    }
+		for _, addr := range addrs {
+			io.WriteString(w, addr.String()+"\n")
+		}
 	}
 }
 
 func main() {
-    Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
-    Info.Println("Started")
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	Info.Println("Started")
 	http.HandleFunc("/", responseWithIPs)
 	http.ListenAndServe(":8000", nil)
 }
