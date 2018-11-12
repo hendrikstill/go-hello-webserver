7dd5f6349e68693bde3594ca8d745ea9bf0cc63e
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2018-11-12 16:20:02 +0100

    Cache iface
    Signed-off-by: Johannes M. Scheuermann <joh.scheuer@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM alpine:3.3
-MAINTAINER Johannes Scheuermann <johannes.scheuermann@inovex.de>
-ADD hello-webserver /hello-webserver
+FROM golang:1.11.2-stretch AS build
+WORKDIR /go/src/github.com/johscheuer/go-hello-webserver
+COPY hello-webserver.go /go/src/github.com/johscheuer/go-hello-webserver/hello-webserver.go
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hello-webserver .
+
+
+FROM scratch
+COPY --from=build /go/src/github.com/johscheuer/go-hello-webserver/hello-webserver /hello-webserver
 CMD ["/hello-webserver"]
 EXPOSE 8000
--- hello-webserver.go
+++ hello-webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -18,7 +19,31 @@ var (
 	Warning *log.Logger
 	//Error logger for error logs
 	Error *log.Logger
+	// ifaceString
+	output *string
 )
+
+func fetchAllIfaces() (*string, error) {
+	result := ""
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return &result, err
+	}
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return &result, err
+		}
+
+		for _, addr := range addrs {
+			result = fmt.Sprint(result, addr.String()+"\n")
+		}
+	}
+
+	return &result, nil
+}
 
 //Init all different logger
 func Init(
@@ -47,24 +72,7 @@ func Init(
 func responseWithIPs(w http.ResponseWriter, r *http.Request) {
 	Info.Printf("Got %q Request on %q", r.Method, r.Host)
 	io.WriteString(w, "Hello my addresses are the following:\n")
-
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		Error.Println(err.Error())
-		http.Error(w, err.Error(), 500)
-	}
-
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			Error.Println(err.Error())
-		}
-
-		for _, addr := range addrs {
-			io.WriteString(w, addr.String()+"\n")
-		}
-	}
+	io.WriteString(w, *output)
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
@@ -73,6 +81,12 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	var err error
+	output, err = fetchAllIfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	Info.Println("Started")
 	http.HandleFunc("/", responseWithIPs)
 	http.HandleFunc("/health", healthCheck)
